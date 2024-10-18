ecd8e68862eb04e771ebcfe3606ad11b0715e2ae
Ganesh Ravichandran <the.ganesh.ravichandran@gmail.com>
2024-10-18 22:20:41 +0000

    nanoGPT

--- training/mnist-multi-gpu/main.py
+++ training/mnist-multi-gpu/main.py
@@ -80,7 +80,8 @@ def demo_basic(local_world_size, local_rank):
     # setup devices for this process. For local_world_size = 2, num_gpus = 8,
     # rank 0 uses GPUs [0, 1, 2, 3] and
     # rank 1 uses GPUs [4, 5, 6, 7].
-    n = torch.cuda.device_count() // local_world_size
+    # n = torch.cuda.device_count() // local_world_size
+    n = 3
     device_ids = list(range(local_rank * n, (local_rank + 1) * n))
 
     print(
